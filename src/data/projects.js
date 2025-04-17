553fd682305be7c8abeb79e0e8ba29ce6b461222
EnryuTrue <noesunselfiesunvideo@gmail.com>
2025-04-17 20:51:23 +0100

    add a daralfarah

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,13 +3,17 @@ import browseEvents from "../assets/Ticketster/browseEvents.jpeg";
 import dashboardMobile from "../assets/ManagmentApp/dashboard_mobile.jpg";
 import projectsMobile from "../assets/ManagmentApp/projects mobile.jpg";
 import statisticMobile from "../assets/ManagmentApp/statistic_mobile.jpg";
+import darelfarah from "../assets/weddingPlace/darelfarah.png";
+import darelfarah2 from "../assets/weddingPlace/darelfarah2.png";
+import darelfarah3 from "../assets/weddingPlace/darelfarah3.png";
+
 
 // Project data with multiple images per project
 const projects = [
   {
     id: 1,
     title: "Event Management Platform",
-    description: "A modern event management platform with intuitive browsing and booking features. Built with React, Node.js, and MongoDB.",
+    description: "A modern event management platform with intuitive browsing and booking features. Built with React, Node.js.",
     category: "Featured",
     mainImage: browseEvents,
     images: [
@@ -29,7 +33,20 @@ const projects = [
       dashboardMobile,
     ],
     technologies: ["React Native", "TailwindCSS", "Firebase", "Expo"],
-  }
+  },
+  {
+    id: 1,
+    title: "wedding place showcase",
+    description: "A wedding place showcase website built with React, Node.js.",
+    category: "Featured",
+    mainImage: darelfarah,
+    images: [
+      darelfarah,
+      darelfarah2,
+      darelfarah3,
+    ],
+    technologies: ["React", "Node.js", "Django", "PostgreSQL", "REST API"],
+  },
 ];
 
 export default projects; 
